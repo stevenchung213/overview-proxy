45288a4b7c528e3fd8271910fa81875345ece4ca
Steven Chung <stevenchung.213@gmail.com>
2018-11-08 15:59:32 -0800

    proxy server updates to account for deployment

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('http://su-casa-overview.us-west-1.elasticbeanstalk.com/', 'public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/:propertyID', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/public/index.html`));
+// });
 
 app.get('/:propertyID', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/public/index.html`));
+  res.sendFile('http://su-casa-overview.us-west-1.elasticbeanstalk.com/index.html');
 });
 
+
+// app.listen(port, () => {
+//   console.log(`server running at http://localhost:${port}`);
+// });
 
 app.listen(port, () => {
-  console.log(`server running at http://localhost:${port}`);
+  console.log(`server running at http://su-casa-overview.us-west-1.elasticbeanstalk.com/`);
 });
+
+// http://su-casa-overview.us-west-1.elasticbeanstalk.com/
